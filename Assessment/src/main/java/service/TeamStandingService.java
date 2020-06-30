abf21f6e3b6fa7e5dd127da632b808e577ea1395
PreetikaArora <67480994+PreetikaArora@users.noreply.github.com>
2020-06-30 18:48:15 +0530

    Updating package

--- Assessment/src/main/java/service/TeamStandingService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import domain.TeamStandingRequest;
-
-import javax.ws.rs.core.Response;
-
-public interface TeamStandingService {
-    Response getTeamStandings(TeamStandingRequest teamStandingRequest);
-}
