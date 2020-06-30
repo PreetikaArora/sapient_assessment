04cd23cd0ca15ce3212d04e28364151fddcece1b
PreetikaArora <67480994+PreetikaArora@users.noreply.github.com>
2020-06-30 18:48:55 +0530

    Updating package

--- Assessment/src/main/java/serviceagent/TeamStandingServiceAgent.java
+++
@@ -1,9 +0,0 @@
-package serviceagent;
-
-import domain.TeamStanding;
-import domain.TeamStandingRequest;
-
-public interface TeamStandingServiceAgent {
-
-    TeamStanding getTeamStandings(TeamStandingRequest teamStandingRequest);
-}
